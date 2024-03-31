a686a40494732d0bf9ec231020879c9c1ce82a52
Zheng <GodTheHands@gmail,com>
2024-03-31 23:32:58 +0800

    new file

---
+++ android/app/src/main/kotlin/com/example/latextb/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.latextb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/splash.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'homePage.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image(
+          image: AssetImage("images/icon.png")
+        ),
+      ),
+    );
+  }
+}
